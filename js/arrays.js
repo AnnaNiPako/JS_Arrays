a03fc12992dcc74767fb0c018d0d1dd9c06b63f9
Anna <an.pvl.gd@gmail.com>
2023-10-27 02:29:50 +0300

    Задача 4 решена

--- js/arrays.js
+++ js/arrays.js
@@ -1,3 +1,5 @@
+// Задача 1
+
 function task1()
 {
     let arr=Array.from(Array(10),()=>{return Math.round(Math.random()*20)});
@@ -6,6 +8,8 @@ function task1()
     console.log(sum);
 }
 
+// Задача 2
+
 function task2()
 {
     let arr=Array.from(Array(10),()=>{return Math.round(Math.random()*20)});
@@ -13,6 +17,8 @@ function task2()
     let sum=arr.filter(index=>(index>0&&index<10)).reduce((sum,elem)=>sum+elem,0);
     console.log(sum);
 }
+
+// Задача 3
 
 function task3()
 {
@@ -27,3 +33,22 @@ function isChetn(arr)
     return arr.every(elem=>elem%2==0);
 }
 
+// Написала функцию создающую массив
+
+function createArray()
+{
+    let arr=Array.from(Array(10),()=>{return Math.round(Math.random()*20)});
+    console.log(arr);
+}
+
+// Задача 4
+
+function getNewArray()
+{
+    let arr=Array.from(Array(10),()=>{return Math.round(Math.random()*20)});
+    console.log(arr);
+    let newarr=arr.filter(elem=>elem%5==0);
+    console.log(newarr);
+}
+
+// Задача 5
